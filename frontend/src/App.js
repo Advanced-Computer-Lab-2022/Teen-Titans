2bf80f93a617320c834e1d020a84676a8ff437e0
Doha-glitch <54781930+Doha-glitch@users.noreply.github.com>
2022-10-28 12:31:33 +0200

    view courses frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import{BrowserRouter, Routes,Route} from 'react-router-dom'
 //pages and components
-import Home from './pages/Home'
+//import Home from './pages/Home'
+import Navbar from './components/Navbar';
+import ViewCoursesPage from './pages/ViewCoursesPage';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <div className="pages">
+      <Navbar/>
+      <div className="pages">       
         <Routes>
           <Route
-          path="/"
-          element= {<Home/>}
+          path='/'
+          element= {<ViewCoursesPage/>}
           />
         </Routes>
       </div>
